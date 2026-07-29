c897a47f08570c003fa34e13d849995d559e1510
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2026-07-29 14:12:04 -0700

    remove analytics link

--- i18n/locales/en-CA.ts
+++ i18n/locales/en-CA.ts
@@ -142,8 +142,6 @@ export default {
       intro: {
         p1: "All registrations of businesses, not-for-profit societies, cooperative associations, personal property and manufactured homes are administered and supported by BC Registries and Online Services.",
         p2: "As our online modernization initiative continues, refer to this page for updates on ways to manage registration information.",
-        p3: "For up-to-date data analytics on our products and services, {link}.",
-        p3Link: "visit our BC Registries Statistics and Insights page",
       },
       exploreByAccountType: {
         title: "Explore services by account type:",
